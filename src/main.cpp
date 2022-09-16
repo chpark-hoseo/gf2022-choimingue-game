94754cec68b6708eac46f1fcb92828478e7763ef
choimingue-game <20191270@game.hoseo.edu>
2022-09-16 15:25:40 +0900

    HelloSDL-분리하기
    실습과 추기실습입니다.
    2022.9.16 <20191270 최민규>

--- src/main.cpp
+++ src/main.cpp
@@ -2,237 +2,59 @@
 //
 // written by changhoonpark@gmail.com
 
-
 #include "main.h"
+#include "stdlib.h"
 #include <SDL2/SDL.h>
 
 
 // 이번 실습때, 쓸 요소 (2주차)
-SDL_Window*		g_pWindow = 0;
-SDL_Renderer*	g_pRenderer = 0;
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
 
-SDL_Texture*	g_pTexureImage;
-SDL_Rect		g_RectImage;
-
-SDL_Texture*	g_pTexureText;
-SDL_Rect 		g_RectText;
-
-TTF_Font*		g_pFont;
-Mix_Chunk*		g_pChunk;
-
-bool			g_bRunning = false;
-bool			g_bLeftMousePressed = false;
-
-bool init();
-void handleInput();
-void update();
-void render();
-
-int main(int argc, char* argv[])
+// 함수를 나누기 (3주차)
+bool g_bRunning = false;		// 윈도우가 제대로 생성됐는지 확인하는 변수
+bool init(const char* Stitle, int xpos, int ypos, int Swidth, int Sheight, int flags)
 {
-	// 기본 자료
-	/*if (!init())
-	{
-		return 1; // something's wrong
-	}
-
-	g_bRunning = true;
-
-	while (g_bRunning)
-	{
-		handleInput();
-		update();
-		render();
-	}
-
-	Mix_CloseAudio();
-	Mix_Quit();
-	TTF_CloseFont(g_pFont);
-	TTF_Quit();
-	SDL_Quit();
-	*/
-
-	// 2주차 실습 내용, 0이면 정상, 음수면 오류
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
-		// 윈도우를 생성, 중심 지점에 640, 480크기로 생성
-		g_pWindow = SDL_CreateWindow("Game Framework",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED,
-			1024, 768,
-			// SDL_WINDOW_BORDERLESS = 빈 공간을 여백으로 채워줌
-			// SDL_WINDOW_MAXIMIZED = 화면을 꽉 채워줌
-			SDL_WINDOW_BORDERLESS | SDL_WINDOW_MAXIMIZED);
-	}
-	// 제대로 윈도우가 생성되었다면, 실행 index는 -1로 설정
-	if (g_pWindow != NULL) {
-		g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-	}
-	// 그게 아니면 종료
-	else
-		return 1;
+		// 윈도우를 생성
+		g_pWindow = SDL_CreateWindow(Stitle, xpos, ypos, Swidth, Sheight, flags);
 
-	// 색깔 지우기(채우기?) = rgb값 + 투명도 (255 : 불투명)
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255);
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
-
-	// 밀리 세컨드 기준 1000 = 1초
-	SDL_Delay(10000);
-	SDL_Quit();
-	return 0;
-}
-
-bool init()
-{
-	SDL_Surface* pTempSurface;
-
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-	{
-		SDL_Log("SDL_Init Error: %s\n", SDL_GetError());
-		return false;
-	}
-
-	g_pWindow = SDL_CreateWindow("HoseoSDL.2022",
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		640, 480, SDL_WINDOW_SHOWN);
-	if (g_pWindow == NULL)
-	{
-		SDL_Log("SDL_CreateWindow Error: %s\n", SDL_GetError());
-		return false;
-	}
-
-	g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-	if (g_pRenderer == NULL)
-	{
-		SDL_Log("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-		return false;
-	}
-	else
-	{
-		// SDL Image
-		pTempSurface = IMG_Load("./assets/animate-alpha.png");
-		if (pTempSurface == NULL)
-		{
-			SDL_Log("IMG_Load Error: %s\n", IMG_GetError());
-			return false;
-		}
+		if (g_pWindow != NULL)
+			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
 		else
-		{
-			g_pTexureImage = SDL_CreateTextureFromSurface(g_pRenderer, pTempSurface);
-			g_RectImage = { 0, 0, 128, 82 };
-			SDL_FreeSurface(pTempSurface);
-		}
-	}
-
-	// SDL TTF 
-	if (TTF_Init() < 0)
-	{
-		SDL_Log("TTF_Init Error: %s\n", TTF_GetError());
-		return false;
-	}
-	else
-	{
-		g_pFont = TTF_OpenFont("./assets/NanumGothic.ttf", 20);
-		if (g_pFont == NULL)
-		{
-			SDL_Log("TTF_OpenFont Error: %s\n", TTF_GetError());
 			return false;
-		}
-		else
-		{
-			// SDL TTF
-#ifdef UNICODE
-			pTempSurface = TTF_RenderUNICODE_Shaded(g_pFont, (Uint16*)L"한글 텍스쳐",
-				SDL_Color{ 0, 0, 255 }, SDL_Color{ 255, 255, 255 });
-#else
-			pTempSurface = TTF_RenderUTF8_Shaded(g_pFont, "한글 텍스쳐",
-				SDL_Color{ 0, 0, 255 }, SDL_Color{ 255, 255, 255 });
-#endif
-			if (pTempSurface == NULL)
-			{
-				SDL_Log("TTF_Render Error: %s\n", TTF_GetError());
-				return false;
-			}
-			else
-			{
-				g_pTexureText = SDL_CreateTextureFromSurface(g_pRenderer, pTempSurface);
-				g_RectText = { 0, 0, pTempSurface->w, pTempSurface->h };
-				SDL_FreeSurface(pTempSurface);
-			}
-		}
-
-	}
-
-	// SDL Mixer 
-	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
-	{
-		SDL_Log("Mix_OpenAudio Error: %s\n", Mix_GetError());
-		return false;
-	}
-	else
-	{
-		g_pChunk = Mix_LoadWAV("./assets/jump.wav");
-		if (g_pChunk == NULL)
-		{
-			SDL_Log("Mix_LoadWAV Error : %s\n", Mix_GetError());
-			return false;
-		}
-		else
-		{
-			Mix_Volume(-1, 128);
-		}
 	}
 
 	return true;
 }
-
-void handleInput()
-{
-	SDL_Event event;
-	while (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-		case SDL_QUIT:
-			g_bRunning = false;
-			break;
-
-		case SDL_MOUSEBUTTONDOWN:
-			if (event.button.button == SDL_BUTTON_LEFT)
-			{
-				g_bLeftMousePressed = true;
-			}
-			break;
-
-		case SDL_MOUSEBUTTONUP:
-			if (event.button.button == SDL_BUTTON_LEFT)
-			{
-				g_bLeftMousePressed = false;
-			}
-			break;
-
-		}
-	}
-}
-
-void update()
-{
-	if (g_bLeftMousePressed)
-	{
-		// chunk에 담긴 사운드 재생, -1은 가장 가까운 채널, 0은 반복 횟수
-		Mix_PlayChannel(-1, g_pChunk, 0);
-		printf("왼쪽클릭 눌림 - 사운드 재생\n");
-	}
-}
-
 void render()
 {
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+	SDL_SetRenderDrawColor(g_pRenderer, rand() % 256, rand() % 256, rand() % 256, 255);
 	SDL_RenderClear(g_pRenderer);
-
-	SDL_RenderCopy(g_pRenderer, g_pTexureImage, &g_RectImage, &g_RectImage);
-	SDL_RenderCopy(g_pRenderer, g_pTexureText, &g_RectText, &g_RectText);
-	filledCircleColor(g_pRenderer, 300, 300, 50, 0xFFFF0000);
-
 	SDL_RenderPresent(g_pRenderer);
+	SDL_Delay(1000);
+}
+
+int main(int argc, char* argv[])
+{
+	int swidth = 640;			// 모니터 폭
+	int sheight = 480;			// 모니터 높이
+	int rCount = 0;				// 렌더링을 실행한 횟수
+	const int out_Count = 10;	// 종료할 수 있는 최소 수치
+
+	if (init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		swidth, sheight, SDL_WINDOW_SHOWN)) {
+		g_bRunning = true;
+	}
+	else
+		return 1;
+
+	// 제대로 생성됐다면, 적용
+	while (g_bRunning)
+	{
+		render();
+	}
+	// 밀리 세컨드 기준 1000 = 1초
+	SDL_Quit();
+	return 0;
 }
