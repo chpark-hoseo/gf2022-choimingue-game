a70b1a6da4dea52f987b6bea4c8da93af72848b8
choimingue-game <20191270@game.hoseo.edu>
2022-09-23 13:10:28 +0900

    Game클래스화 -과제-
    20191270최민규
    과제내용입니다.
    감사합니다.

---
+++ src/GameClass.cpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "GameClass.h"
+#include "main.h"
+
+bool GameClass::init(const char* Stitle, int xpos, int ypos, int Swidth, int Sheight, int flags)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+		// 윈도우를 생성
+		m_pWindow = SDL_CreateWindow(Stitle, xpos, ypos, Swidth, Sheight, flags);
+
+		if (m_pWindow != NULL) {
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+			if (m_pRenderer != NULL)
+				SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
+			else
+				return false;
+		}
+		else
+			return false;
+	}
+	else
+		return false;
+
+	m_bRunning = true;				// 정상작동
+	return m_bRunning;
+}
+
+void GameClass::update()
+{
+	// 게임 진행 내용
+}
+
+void GameClass::renderer()
+{
+	SDL_RenderClear(m_pRenderer);
+	SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);				// 검은색으로 표시
+	SDL_RenderPresent(m_pRenderer);
+}
+
+bool GameClass::running()
+{
+	return m_bRunning;
+}
+
+void GameClass::handleEvent()
+{
+	SDL_Event gm_event;
+
+	while (SDL_PollEvent(&gm_event)) {			// 이벤트 진행시
+		switch (gm_event.type)
+		{
+		case SDL_QUIT:							// 사용자가 종료를 원할시 종료
+			m_bRunning = false;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void GameClass::clean()
+{
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,206 +2,38 @@
 //
 // written by changhoonpark@gmail.com
 
+// 과제 1. << if -> while로 바꾸고 이유를 주석으로 추가 >>
+
+/* if문을 사용한 경우, if문 밖으로 나갔을때, 사용자가 입력을 하게 된다면, 키가 씹히기 때문에
+ while문을 사용하는 것이 바람직하다.*/
+
+ // 과제 2. << 헤더 파일 중복과 순환참조의 차이점과 해결 방법 >>
+
+ /* 헤더 파일 중복, 순환 참조시켜 컴파일시 속도가 느려지는 등의 문제가 발생하게 된다.
+  이러한 이유로 인해 전방 선언이나, 인터페이스의 활용, 전처리기등을 활용하여 이를 막는다.
+  특히, 전방 선언같은 경우에는 클래스의 존재 여부만 알기에 포인터로만 생성이 가능하다.*/
 
 #include "main.h"
-
-
-SDL_Window*		g_pWindow = 0;
-SDL_Renderer*	g_pRenderer = 0;
-
-SDL_Texture*	g_pTexureImage;
-SDL_Rect		g_RectImage;
-
-SDL_Texture*	g_pTexureText;
-SDL_Rect 		g_RectText;
-
-TTF_Font*		g_pFont;
-Mix_Chunk*		g_pChunk;
-
-bool			g_bRunning = false;
-bool			g_bLeftMousePressed = false;
-
-bool init();
-void handleInput();
-void update();
-void render();
+#include "GameClass.h"
 
 int main(int argc, char* argv[])
 {
-	if (!init())
+	GameClass* game = new GameClass;
+	bool g_brunning;
+
+	int s_width = 640;				// 실행할 프로그램의 길이
+	int s_heigh = 480;				//					 높이
+
+	g_brunning = game->init("GameClass", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+
+	// 게임의 창이 띄워졌는지 확인5
+	if (!g_brunning)
+		return 0;
+
+	while (game->running())
 	{
-		return 1; // something's wrong
+		game->handleEvent();
+		game->update();
+		game->renderer();
 	}
-
-	g_bRunning = true;
-
-	while (g_bRunning)
-	{
-		handleInput();
-		update();
-		render();
-	}
-
-	Mix_CloseAudio();
-	Mix_Quit();
-	TTF_CloseFont(g_pFont);
-	TTF_Quit();
-	SDL_Quit();
-
-	return 0;
-}
-
-bool init()
-{
-	SDL_Surface* pTempSurface;
-
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-	{
-		SDL_Log("SDL_Init Error: %s\n", SDL_GetError());
-		return false;
-	}
-
-	g_pWindow = SDL_CreateWindow("HoseoSDL.2022",
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		640, 480, SDL_WINDOW_SHOWN);
-	if (g_pWindow == NULL)
-	{
-		SDL_Log("SDL_CreateWindow Error: %s\n", SDL_GetError());
-		return false;
-	}
-
-	g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-	if (g_pRenderer == NULL)
-	{
-		SDL_Log("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-		return false;
-	}
-	else
-	{
-		// SDL Image
-		pTempSurface = IMG_Load("./assets/animate-alpha.png");
-		if (pTempSurface == NULL)
-		{
-			SDL_Log("IMG_Load Error: %s\n", IMG_GetError());
-			return false;
-		}
-		else
-		{
-			g_pTexureImage = SDL_CreateTextureFromSurface(g_pRenderer, pTempSurface);
-			g_RectImage = { 0, 0, 128, 82 };
-			SDL_FreeSurface(pTempSurface);
-		}
-	}
-
-	// SDL TTF 
-	if (TTF_Init() < 0)
-	{
-		SDL_Log("TTF_Init Error: %s\n", TTF_GetError());
-		return false;
-	}
-	else
-	{
-		g_pFont = TTF_OpenFont("./assets/NanumGothic.ttf", 20);
-		if (g_pFont == NULL)
-		{
-			SDL_Log("TTF_OpenFont Error: %s\n", TTF_GetError());
-			return false;
-		}
-		else
-		{
-			// SDL TTF
-#ifdef UNICODE
-			pTempSurface = TTF_RenderUNICODE_Shaded(g_pFont, (Uint16*)L"한글 텍스쳐",
-				SDL_Color{ 0, 0, 255 }, SDL_Color{ 255, 255, 255 });
-#else
-			pTempSurface = TTF_RenderUTF8_Shaded(g_pFont, "한글 텍스쳐",
-				SDL_Color{ 0, 0, 255 }, SDL_Color{ 255, 255, 255 });
-#endif
-			if (pTempSurface == NULL)
-			{
-				SDL_Log("TTF_Render Error: %s\n", TTF_GetError());
-				return false;
-			}
-			else
-			{
-				g_pTexureText = SDL_CreateTextureFromSurface(g_pRenderer, pTempSurface);
-				g_RectText = { 0, 0, pTempSurface->w, pTempSurface->h };
-				SDL_FreeSurface(pTempSurface);
-			}
-		}
-
-	}
-
-	// SDL Mixer 
-	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
-	{
-		SDL_Log("Mix_OpenAudio Error: %s\n", Mix_GetError());
-		return false;
-	}
-	else
-	{
-		g_pChunk = Mix_LoadWAV("./assets/jump.wav");
-		if (g_pChunk == NULL)
-		{
-			SDL_Log("Mix_LoadWAV Error : %s\n", Mix_GetError());
-			return false;
-		}
-		else
-		{
-			Mix_Volume(-1, 128);
-		}
-	}
-
-	return true;
-}
-
-void handleInput()
-{
-	SDL_Event event;
-	while (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-		case SDL_QUIT:
-			g_bRunning = false;
-			break;
-
-		case SDL_MOUSEBUTTONDOWN:
-			if (event.button.button == SDL_BUTTON_LEFT)
-			{
-				g_bLeftMousePressed = true;
-			}
-			break;
-
-		case SDL_MOUSEBUTTONUP:
-			if (event.button.button == SDL_BUTTON_LEFT)
-			{
-				g_bLeftMousePressed = false;
-			}
-			break;
-
-		}
-	}
-}
-
-void update()
-{
-	if (g_bLeftMousePressed)
-	{
-		// chunk에 담긴 사운드 재생, -1은 가장 가까운 채널, 0은 반복 횟수
-		Mix_PlayChannel(-1, g_pChunk, 0);
-		printf("왼쪽클릭 눌림 - 사운드 재생\n");
-	}
-}
-
-void render()
-{
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-	SDL_RenderClear(g_pRenderer);
-
-	SDL_RenderCopy(g_pRenderer, g_pTexureImage, &g_RectImage, &g_RectImage);
-	SDL_RenderCopy(g_pRenderer, g_pTexureText, &g_RectText, &g_RectText);
-	filledCircleColor(g_pRenderer, 300, 300, 50, 0xFFFF0000);
-
-	SDL_RenderPresent(g_pRenderer);
 }
