769b5928c7e5a1f4cf89a90079c99e4628eb7b48
choimingue-game <20191270@game.hoseo.edu>
2022-10-04 02:39:19 +0900

    Game_Texture

---
+++ include/Game.h
@@ -0,0 +1,51 @@
+#pragma once
+#include"game.h"
+#include "main.h"
+
+class Game
+{
+public:
+	// 윈도우 크기
+	const int SCREEN_WIDTH = 640;
+	const int SCREEN_HEIGHT = 480;
+
+	// 윈도우 생성
+	bool init(const char* Stitle, int xpos, int ypos, int Swidth, int Sheight, int flags);
+
+	// 게임 내용 수정
+	void update();
+
+	// 화면 띄우기
+	void renderer();
+
+	// 정상작동중인지 확인
+	bool running();
+
+	// 종료 설정
+	void handleEvent();
+
+	// 화면 지우기
+	void clean();
+
+private:
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+
+	SDL_Texture* m_pTexture;
+	SDL_Rect m_srcRect;			// 원본 사각형
+	SDL_Rect m_disRect;			// 대상 사각형
+
+	// 이미지를 받아오는 함수 + bmp는 0, png는 1이다.
+	SDL_Texture* Text_Maker(const char* Par_Objname, SDL_Rect* scr, SDL_Rect* dis, int extDif);
+
+	int ext_bmp = 0;
+	int ext_png = 1;
+
+	int obj_Speed = 1;										// 물체가 움직이는 속도 = 1
+	int nChgWay_Cnt = 0;									// 현재 방향이 바뀐 횟수
+
+	const char* adr_Char = "assets/Char.png";				// 캐릭터의 그림 주소
+	const char* adr_Bg = "assets/Back_Ground.png";			// 배경 그림의 주소
+	const char* adr_Rider = "assets/rider.bmp";				// 라이더 그림의 주소
+};
---
+++ src/Game.cpp
@@ -0,0 +1,123 @@
+#include "Game.h"
+#include "main.h"
+
+bool Game::init(const char* Stitle, int xpos, int ypos, int Swidth, int Sheight, int flags)
+{
+	// 윈도우를 생성
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+		m_pWindow = SDL_CreateWindow(Stitle, xpos, ypos, Swidth, Sheight, flags);
+
+		if (m_pWindow != NULL) {
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+			if (m_pRenderer != NULL)
+				SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
+			else
+				return false;
+		}
+		else
+			return false;
+	}
+	else
+		return false;
+
+	m_bRunning = true;				// 정상작동
+
+	m_pTexture = Text_Maker(adr_Rider, &m_srcRect, &m_disRect, 0);
+
+	if (m_pTexture == nullptr)
+		std::cout << "1";
+
+	return m_bRunning;
+}
+
+SDL_Texture* Game::Text_Maker(const char* Par_Objname, SDL_Rect* scr, SDL_Rect* dis, int extDif)
+{
+	SDL_Surface* ptSurface = NULL;
+	SDL_Texture* texture = NULL;
+
+	if (extDif == ext_bmp)
+		ptSurface = SDL_LoadBMP(Par_Objname);							// 그림 데이터를 가져옴
+	else
+		ptSurface = IMG_Load(Par_Objname);
+
+	texture = SDL_CreateTextureFromSurface(m_pRenderer, ptSurface);			// 가져온 그림 데이터를 가져옴
+	SDL_FreeSurface(ptSurface);
+
+	SDL_QueryTexture(texture, NULL, NULL, &scr->w, &scr->h);				// 원본 그림의 크기를 가져오기
+
+	dis->w = scr->w;		// 123
+	dis->h = scr->h;		// 87
+
+	dis->x = scr->x = 0;
+	dis->y = scr->y = 0;
+
+	return texture;
+}
+
+// 참고 자료 : https://gamdekong.tistory.com/173
+
+void Game::update()
+{
+	SDL_Delay(10);
+	// 게임 진행 내용
+}
+
+void Game::renderer()
+{
+	const int chgWay_Max = 640 - m_srcRect.w;			// 물체의 방향이 바뀌는 횟수
+
+	SDL_RenderClear(m_pRenderer);
+
+	SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 0);
+
+
+
+	/* 4번째 과제 = SDL_RenderCopyEx 를 이용한 다양한 예제 만들기 */
+	// 오른쪽으로 이동중
+	if (obj_Speed >= 0)
+		SDL_RenderCopyEx(m_pRenderer, m_pTexture, &m_srcRect, &m_disRect, 0, NULL, SDL_FLIP_HORIZONTAL);
+	else
+		SDL_RenderCopyEx(m_pRenderer, m_pTexture, &m_srcRect, &m_disRect, 0, NULL, SDL_FLIP_NONE);
+
+	if (nChgWay_Cnt > chgWay_Max) {
+		obj_Speed = -obj_Speed;
+		nChgWay_Cnt = 0;
+	}
+
+	m_disRect.x += obj_Speed;
+	nChgWay_Cnt++;
+
+	SDL_RenderPresent(m_pRenderer);
+}
+
+bool Game::running()
+{
+	return m_bRunning;
+}
+
+void Game::handleEvent()
+{
+	SDL_Event gm_event;
+
+	while (SDL_PollEvent(&gm_event)) {			// 이벤트 진행시
+		switch (gm_event.type)
+		{
+		case SDL_QUIT:							// 사용자가 종료를 원할시 종료
+			m_bRunning = false;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void Game::clean()
+{
+	SDL_DestroyWindow(m_pWindow);
+
+	SDL_DestroyRenderer(m_pRenderer);
+
+	SDL_DestroyTexture(m_pTexture);
+
+	SDL_Quit();
+}
